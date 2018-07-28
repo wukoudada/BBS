e14036cc55b07d297cd36c01dd45ef903fc38f9f
liziczh <liziczh@foxmail.com>
2018-07-28 09:43:21 +0800

    Readme

--- src/com/panda/bbs/dao/IPostDao.java
+++ src/com/panda/bbs/dao/IPostDao.java
@@ -2,11 +2,23 @@ package com.panda.bbs.dao;
 
 import com.panda.bbs.domain.Post;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IPostDao {
-    public List<Post> getAllPost();
-    public Post getPostById(int postId);
-    public List<Post> getPostByTitle();
+    // 获取所有文章
+    public List<Post> getAllPost() throws SQLException;
+    // 根据文章ID获取文章
+    public Post getPostByPostId(int postId) throws SQLException;
+    // 根据作者ID获取文章
+    public List<Post> getPostByUserId(int userId) throws SQLException;
+    // 根据文章标题模糊查询
+    public List<Post> getPostByTitle(String title) throws SQLException;
+    // 新增一篇文章
+    public void insertPost(Post post) throws SQLException;
+    // 更新一篇文章
+    public void updatePost(Post post) throws SQLException;
+    // 删除一篇文章
+    public void deletePostById(int id) throws SQLException;
 
 }
--- src/com/panda/bbs/dao/impl/PostDaoImpl.java
+++ src/com/panda/bbs/dao/impl/PostDaoImpl.java
@@ -2,24 +2,60 @@ package com.panda.bbs.dao.impl;
 
 import com.panda.bbs.dao.IPostDao;
 import com.panda.bbs.domain.Post;
+import com.panda.bbs.utils.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class PostDaoImpl implements IPostDao {
-
+    QueryRunner qr = new QueryRunner(JDBCUtils.getDataSource());
     @Override
-    public List<Post> getAllPost() {
-
-        return null;
+    public List<Post> getAllPost() throws SQLException {
+        String sql = "select ";
+        List<Post> postList = qr.query(sql,new BeanListHandler<Post>(Post.class));
+        return postList;
     }
 
     @Override
-    public Post getPostById(int postId) {
-        return null;
+    public Post getPostByPostId(int postId) throws SQLException {
+        String sql = "";
+        Post post = qr.query(sql,new BeanHandler<Post>(Post.class));
+        return post;
     }
 
     @Override
-    public List<Post> getPostByTitle() {
-        return null;
+    public List<Post> getPostByUserId(int userId) throws SQLException {
+        String sql = "";
+        List<Post> postList = qr.query(sql,new BeanListHandler<Post>(Post.class));
+        return postList;
     }
+
+    @Override
+    public List<Post> getPostByTitle(String title) throws SQLException {
+        String sql = "";
+        List<Post> postList = qr.query(sql,new BeanListHandler<Post>(Post.class));
+        return postList;
+    }
+
+    @Override
+    public void insertPost(Post post) throws SQLException {
+        String sql = "";
+        qr.update(sql);
+    }
+
+    @Override
+    public void updatePost(Post post) throws SQLException {
+        String sql = "";
+        qr.update(sql);
+    }
+
+    @Override
+    public void deletePostById(int id) throws SQLException {
+        String sql = "";
+        qr.update(sql,id);
+    }
+
 }
